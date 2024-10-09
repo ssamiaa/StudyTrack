9a9a8fef4eac5a0158b18cdfeb2cb0458d16401e
samia <151353023+ssamiaa@users.noreply.github.com>
2024-10-08 22:38:25 -0700

    Revert "Added Implentation for Lesson Objective Class"
    This reverts commit 8426a769b1bdb9e6e017edf222cc48aa8351a2d7.

--- src/main/model/LessonObjective.java
+++ src/main/model/LessonObjective.java
@@ -1,50 +1,66 @@
 package model;
 
-//Represents a single lesson objective within a topic.
+/**
+ * Represents a single lesson objective within a topic.
+ */
 public class LessonObjective {
     private String description;
     private boolean isMastered;
 
-    // Constructs a LessonObjective with the given description.
-    // REQUIRES: description is a non-empty string.
-    // EFFECTS: Initializes the lesson objective with the provided description and sets isMastered to false.
+    /**
+     * Constructs a LessonObjective with the given description.
+     *
+     * REQUIRES: description is a non-empty string.
+     * EFFECTS: Initializes the lesson objective with the provided description and sets isMastered to false.
+     */
     public LessonObjective(String description) {
-        this.description = description;
-        this.isMastered = false;
     }
 
-    // Returns the description of the lesson objective.
+    /**
+     * Returns the description of the lesson objective.
+     *
+     * @return description of the lesson objective.
+     */
     public String getDescription() {
         return description;
     }
 
-    // Checks if the lesson objective is mastered.
+    /**
+     * Checks if the lesson objective is mastered.
+     *
+     * @return true if mastered, false otherwise.
+     */
     public boolean isMastered() {
         return isMastered;
     }
 
-    
-    // Marks the lesson objective as mastered.
-    // MODIFIES: this
-    // EFFECTS: Sets isMastered to true.
+    /**
+     * Marks the lesson objective as mastered.
+     *
+     * MODIFIES: this
+     * EFFECTS: Sets isMastered to true.
+     */
     public void markAsMastered() {
         this.isMastered = true;
     }
 
-    // Unmarks the lesson objective as mastered.
-    // MODIFIES: this
-    // EFFECTS: Sets isMastered to false.
+    /**
+     * Unmarks the lesson objective as mastered.
+     *
+     * MODIFIES: this
+     * EFFECTS: Sets isMastered to false.
+     */
     public void unmarkAsMastered() {
         this.isMastered = false;
     }
 
-    // Returns a string representation of the lesson objective.
+    /**
+     * Returns a string representation of the lesson objective.
+     *
+     * @return String representation.
+     */
     @Override
     public String toString() {
-        String masteryStatus = " [Not Mastered]";
-        if (isMastered) {
-            masteryStatus = " [Mastered]";
-        }
-        return description + masteryStatus;
+        return description;
     }
 }
